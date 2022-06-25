f62e47b6c94dc89f53cd2f82e9b2baea0a87e091
Lucas Duarte <lukazds2016@gmail.com>
2022-06-25 16:01:23 -0300

    Retornando as dez últimas mensagens recentes

--- src/index.js
+++ src/index.js
@@ -19,27 +19,28 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-const userSchema = joi.object({ name: joi.string().required() }); 
+const userSchema = joi.object({ name: joi.string().required() });
 
-const messageSchema = joi.object({ 
-        to: joi.string().required(),
-        text: joi.string().required(),
-        type: joi.string().required(),
-        from: joi.string().required()}); 
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required(),
+    from: joi.string().required()
+});
 
 server.get("/participants", async (req, res) => {
 
     const nowUsers = await db.collection('users').find().toArray();
     res.send(nowUsers)
-    
+
 })
 
 server.post("/participants", async (req, res) => {
     const userName = req.body;
 
-    const validation = userSchema.validate(userName, {abortEarly: true});
+    const validation = userSchema.validate(userName, { abortEarly: true });
 
-    if(validation.error) {
+    if (validation.error) {
         res.sendStatus(422);
         return;
     }
@@ -48,25 +49,26 @@ server.post("/participants", async (req, res) => {
 
         const nowUsers = await db.collection('users').find().toArray();
 
-        if(nowUsers.some(v => v.name === userName.name)) {
+        if (nowUsers.some(v => v.name === userName.name)) {
             res.sendStatus(409);
             return;
         }
 
-        await db.collection("users").insertOne({...userName, lastStatus: Date.now()});
-        
+        await db.collection("users").insertOne({ ...userName, lastStatus: Date.now() });
+
         const time = `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
 
         await db.collection("messages").insertOne({
-                from: userName.name, 
-                to: 'Todos', 
-                text: 'entra na sala...', 
-                type: 'status', 
-                time: time})
-        
+            from: userName.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: time
+        })
+
         res.sendStatus(201);
 
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 
@@ -74,12 +76,15 @@ server.post("/participants", async (req, res) => {
 
 server.get("/messages", async (req, res) => {
 
-    const limit = req.query.limit;
     const nowMessages = await db.collection('messages').find().toArray();
 
-    console.log(limit)
-    res.send(nowMessages)
-    
+    const limit = (req.query.limit !== undefined) 
+        ? Number(req.query.limit) : nowMessages.length;
+
+    nowMessages.reverse()
+
+    res.send(nowMessages.slice(0, limit))
+
 })
 
 server.post("/messages", async (req, res) => {
@@ -90,20 +95,20 @@ server.post("/messages", async (req, res) => {
 
     const types = ["message", "private_message"];
 
-    if(!nowUsers.some(v => v.name === from)) {
+    if (!nowUsers.some(v => v.name === from)) {
         res.sendStatus(422);
         return;
     }
 
-    if(!types.some(v => v === partMessage.type)) {
+    if (!types.some(v => v === partMessage.type)) {
         res.sendStatus(422);
         return;
     }
 
-    const message = {...partMessage, from};
-    const validation = messageSchema.validate(message, {abortEarly: true})
+    const message = { ...partMessage, from };
+    const validation = messageSchema.validate(message, { abortEarly: true })
 
-    if(validation.error) {
+    if (validation.error) {
         res.sendStatus(422);
         return;
     }
@@ -111,11 +116,11 @@ server.post("/messages", async (req, res) => {
     try {
 
         const time = `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
-        await db.collection("messages").insertOne({...message, time})
+        await db.collection("messages").insertOne({ ...message, time })
 
         res.sendStatus(201);
 
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 })
