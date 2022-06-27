c47611929ec13374ba6f76bb47ea642baea33ef0
Lucas Duarte <lukazds2016@gmail.com>
2022-06-27 14:59:00 -0300

    Recebendo nome do usuário atraves do headers da rota /status

--- src/index.js
+++ src/index.js
@@ -129,4 +129,11 @@ server.post("/messages", async (req, res) => {
     }
 })
 
+server.post("/status", async (req, res) => {
+    const name = req.headers.user;
+    const nowUsers = await db.collection("users").find().toArray();
+    
+    res.send(name);
+})
+
 server.listen(5000);
